45d0c12e4df10e62ed1c7449a216317306cf6071
arpithasureshappa <arpitha.sureshappa@tarento.com>
2024-10-15 20:48:23 +0530

    event state read api added

--- src/utils/whitelistApis.ts
+++ src/utils/whitelistApis.ts
@@ -4362,6 +4362,13 @@ export const API_LIST = {
                 ROLE.PUBLIC,
             ],
         },
+        '/proxies/v8/user/event/state/read': {
+            checksNeeded: [CHECK.ROLE],
+            // tslint:disable-next-line: object-literal-sort-keys
+            ROLE_CHECK: [
+              ROLE.PUBLIC,
+            ],
+        },
     },
     URL_PATTERN:
         [
@@ -4926,5 +4933,6 @@ export const API_LIST = {
             '/proxies/v8/cqfquestionset/read/:assessmentIdentifier/:contentId/:versionKey',
             '/proxies/v8/questionset/autoPublish/:id',
             '/proxies/v8/searchBy/:version/:key',
+            '/proxies/v8/user/event/state/read',
            ],
 }
